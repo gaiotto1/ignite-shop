4f5abacb6e65b84f0bc6afd686c786f8b5515c31
Julio Gaiotto <juliogaiotto@hotmail.com>
2023-03-10 11:03:55 -0300

    feat: config stitches in project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 10,
+
+  span: {
+    fontWeight: 700,
+    color: 'red',
+  },
+
+  '&:hover': {
+    backgroundColor: 'red',
+    
+    span: {
+      color: 'white',
+    }
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>
+      <span>Hello World</span>
+    </Button>
   )
 }
