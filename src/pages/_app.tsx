e5bdb329a2e484e7c11cff35fa681f2e2a05ab0c
Julio Gaiotto <juliogaiotto@hotmail.com>
2023-03-20 10:57:40 -0300

    feat: cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
-globalStyles()
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from '../styles/pages/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  globalStyles()
+
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="Logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
